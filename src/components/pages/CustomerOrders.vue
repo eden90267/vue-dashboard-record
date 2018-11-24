11760870b3f4e8abfe021d465b1a500a73177d2d
eden90267 <eden90267@gmail.com>
2018-11-24 15:10:10 +0800

    Import CustomerOrders

--- src/components/pages/CustomerOrders.vue
+++ src/components/pages/CustomerOrders.vue
@@ -115,6 +115,46 @@
         </div>
       </div>
     </div>
+    <div class="my-5 row justify-content-center">
+      <form class="col-md-6" @submit.prevent="createOrder">
+        <div class="form-group">
+          <label for="useremail">Email</label>
+          <input type="email" class="form-control" name="email" id="useremail"
+                 v-model="form.user.email" placeholder="請輸入 Email" v-validate="'required|email'">
+          <span class="text-danger" v-if="errors.has('email')">
+            {{errors.first('email')}}
+          </span>
+        </div>
+
+        <div class="form-group">
+          <label for="username">收件人姓名</label>
+          <input type="text" class="form-control" name="name" id="username"
+                 :class="{'is-invalid': errors.has('name')}"
+                 v-model="form.user.name" placeholder="輸入姓名" v-validate="'required'">
+          <span class="text-danger" v-if="errors.has('name')">姓名必須輸入</span>
+        </div>
+
+        <div class="form-group">
+          <label for="usertel">收件人電話</label>
+          <input type="tel" class="form-control" id="usertel" v-model="form.user.tel" placeholder="請輸入電話">
+        </div>
+
+        <div class="form-group">
+          <label for="useraddress">收件人地址</label>
+          <input type="address" class="form-control" name="address" id="useraddress" v-model="form.user.address"
+                 placeholder="請輸入地址" v-validate="'required'" :class="{'is-invalid': errors.has('address')}">
+          <span class="text-danger" v-if="errors.has('address')">{{errors.first('address')}}</span>
+        </div>
+
+        <div class="form-group">
+          <label for="useraddress">留言</label>
+          <textarea name="" id="" class="form-control" cols="30" rows="10" v-model="form.message"></textarea>
+        </div>
+        <div class="text-right">
+          <button class="btn btn-danger">送出訂單</button>
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
@@ -128,11 +168,20 @@
         products: [],
         product: {},
         cart: {},
+        form: {
+          user: {
+            name: '',
+            email: '',
+            tel: '',
+            address: ''
+          },
+          message: '',
+        },
         isLoading: false,
         status: {
           loadingItem: '', // 產品 id
         },
-        coupon_code: ''
+        coupon_code: '',
       }
     },
     methods: {
@@ -212,7 +261,35 @@
           }
           vm.isLoading = false;
         })
-      }
+      },
+      createOrder() {
+        const vm = this;
+        this.$validator.validate().then((result) => {
+          if (result) {
+            const api = `${process.env.API_PATH}/api/${process.env.CUSTOM_PATH}/order`;
+            const order = vm.form;
+            vm.isLoading = true;
+            vm.$http.post(api, {data: order}).then((res) => {
+              if (res.data.success) {
+                vm.getCart();
+                vm.form = {
+                  user: {
+                    name: '',
+                    email: '',
+                    tel: '',
+                    address: ''
+                  },
+                  message: '',
+                };
+              } else {
+                vm.$bus.$emit('message:push', res.data.message, 'danger');
+              }
+              vm.isLoading = false;
+              vm.$validator.reset();
+            })
+          }
+        });
+      },
     },
     created() {
       this.getProducts();
--- src/main.js
+++ src/main.js
@@ -4,8 +4,10 @@ import Vue from 'vue'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import 'bootstrap'
-import Loading from 'vue-loading-overlay';
-import 'vue-loading-overlay/dist/vue-loading.css';
+import Loading from 'vue-loading-overlay'
+import 'vue-loading-overlay/dist/vue-loading.css'
+import VeeValidate from 'vee-validate'
+import zhTWValidate from 'vee-validate/dist/locale/zh_TW'
 
 import App from './App'
 import router from './router'
@@ -15,6 +17,9 @@ import currencyFilter from './filters/currency'
 Vue.config.productionTip = false
 
 Vue.use(VueAxios, axios)
+
+Vue.use(VeeValidate)
+VeeValidate.Validator.localize('zh_TW', zhTWValidate)
 
 axios.defaults.withCredentials = true
 
